e2cda6822ad4671c902a76a7dde6b0d95686998c
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2024-03-23 16:15:08 -0500

    correct bat package name depending on distro

--- zsh/.config/zsh/plugins/zsh-package-check/zsh-package-check.plugin.zsh
+++ zsh/.config/zsh/plugins/zsh-package-check/zsh-package-check.plugin.zsh
@@ -2,10 +2,19 @@ is_command() {
   command -v $1 &>/dev/null
 }
 
+# On arch, the package is called bat
 if is_command bat; then
+  export BAT_THEME=base16
+  alias cat=bat
+fi
+
+# but on debian, the package is called batcat
+if is_command batcat; then
   export BAT_THEME=base16
   alias cat=batcat
 fi
+
+# dont ask me why ¯\_(ツ)_/¯
 
 if is_command hx || is_command helix; then
   alias hx=helix
